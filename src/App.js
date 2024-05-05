634d727cd60d83bfd070d951dfa36de74f19a24c
skarn3 <skarn@emich.edu>
2024-05-04 20:10:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from "react";
+import HomePage from './Components/HomePage/HomePage';
+function App() {
+
+  // Keep All The Routes Here , use camelCase naming format
+  let routes;
+  routes = (
+    <React.Fragment>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/anotherRoute" element={<HomePage />} /> //replace route and element's component as needed
+    </React.Fragment>
+  );
+  return (
+    <div className="App text-gray-500 ml-40">
+      <Router>
+        <Routes>
+          {routes}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <div className=' text-red-400'>
+                Hello World
+        </div>
+    )
+}
