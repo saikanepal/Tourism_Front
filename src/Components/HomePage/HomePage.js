ac6178e6b3bf3eb423a8feba6acbde5feaa55d7b
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-06 19:51:50 +0545

    first commit

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar/Navbar";
 
 export default function HomePage() {
-    return (
-        <div className=' text-red-400'>
-                Hello World
-        </div>
-    )
+  return (
+    <div className=" text-red-400">
+      <Navbar />
+    </div>
+  );
 }
