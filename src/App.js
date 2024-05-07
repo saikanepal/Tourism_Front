c9ef425007e512ddac8895398e3025c4620ee960
skarn3 <saikanepal@gmail.com>
2024-05-07 08:08:43 +0545

    Sahil : Updated app.js and also added Approuter for better management of routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import React from "react";
-import HomePage from './Components/HomePage/HomePage';
-function App() {
+import React from 'react';
+import AppRouter from './AppRouter';
 
-  // Keep All The Routes Here , use camelCase naming format
-  let routes;
-  routes = (
-    <React.Fragment>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/anotherRoute" element={<HomePage />} /> //replace route and element's component as needed
-    </React.Fragment>
-  );
+function App() {
   return (
     <div className="App text-gray-500 ml-40">
-      <Router>
-        <Routes>
-          {routes}
-        </Routes>
-      </Router>
+      <AppRouter />
     </div>
   );
 }
