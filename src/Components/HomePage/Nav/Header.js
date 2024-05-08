b0c1e1e568b1161243410c30a8e51b61dd07ef5d
skarn3 <saikanepal@gmail.com>
2024-05-08 11:17:11 +0545

    added padding in discovery section

--- src/Components/HomePage/Nav/Header.js
+++ src/Components/HomePage/Nav/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
           //       height: "screen",
           //     },
           //   }}
-          className={` resNav md:flex md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all  :bg-white duration-500 ease-in ${
+          className={` resNav md:flex md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-5] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all  :bg-white duration-500 ease-in ${
             open ? "top-12" : "top-[-490px]"
           }`}
         >
