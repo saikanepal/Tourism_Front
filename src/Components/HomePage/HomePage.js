a02c66cf9d5d6ddacf6c1342c525942c38ef939d
rose-rawal <rooserwl@gmail.com>
2024-05-08 18:37:36 +0545

    final caro push rose

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -9,7 +9,7 @@ import Trekking from '../Trekking/Trekking'
 export default function HomePage() {
     return (
 
-        <div className='font-jomolhari font-semibold text-5xl text-gray-800'>
+        <div className='font-jomolhari font-semibold text-gray-800'>
             <HeroSection />
             <Header />
             <Expedition />
