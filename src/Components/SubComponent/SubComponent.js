8149d02d1d02ba08258776fdc88f34fc698dbaca
ByapakSigdel <sigdelmb123@gmail.com>
2024-05-09 22:01:37 +0545

    Added the form

--- src/Components/SubComponent/SubComponent.js
+++ src/Components/SubComponent/SubComponent.js
@@ -2,13 +2,15 @@
 import HeroSectionSub from "./HeroSection";
 import Header from "../HomePage/Nav/Header.js";
 import React from 'react'
+import ContactForm from '../InnerSub/ContactForm/ContactForm.js'
 
 export default function SubComponent() {
     return (
         <div>
-                SubComponent
+        {/*         SubComponent
         <Header/>
-            <HeroSectionSub />
+            <HeroSectionSub /> */}
+            <ContactForm />
         </div>
     )
 
