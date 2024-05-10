bd13e5effdef30f556f4a7b9ff4f46eb447de9f0
rose-rawal <rooserwl@gmail.com>
2024-05-10 22:10:57 +0545

    carosel edit

--- src/Components/SubComponent/TrekkingAll/TrekkingAll.js
+++ src/Components/SubComponent/TrekkingAll/TrekkingAll.js
@@ -5,6 +5,8 @@ import Caroussel from '../../Carousel/Carousel'
 import {useNavigate,useLocation} from 'react-router-dom'
 import expeditions from '../../../Assets/Data/Expedition'
 import { FaAngleRight } from "react-icons/fa";
+import InnerCarosel from './InnerCarosel/InnerCarosel'
+
 const TrekkingAll = () => {
     const navigate=useNavigate()
   const location=useLocation();
@@ -19,14 +21,16 @@ const TrekkingAll = () => {
         
             {
                 Object.entries(location.pathname==='/trekking'?trekking.trekking:expeditions.expeditions).map((data,i)=>{
-                    return <div className={`justify-center text-center w-full flex items-center lg:justify-between ${(i%2)?'flex-row-reverse':''}`}>
-                                <div className='w-1/2 flex gap-3 justify-center pb-20 lg:pb-0'>
+                    return <div className={` w-full flex justify-start lg:justify-between gap-5`} >
+                                <div className='w-1/2 flex gap-3 justify-center pb-20 lg:pb-0 h-full'>
                                     {/* <h3>{regionImage[i]}</h3> */}
                                     {/* <img src={exp} className='w-32 h-32'/> */}
-                                    <div className='flex justify-center flex-col items-center gap-10 '>
+                                    <div className='flex justify-center flex-col  gap-10 '>
                                         <h1 className='text-4xl text-bold text-custom-gold'>{data[0]}</h1>
-                                        {/* <p>{Object.values(data[1])[0].overview}</p> */}
+                                        <p>{Object.values(data[1])[0].overview}</p>
+                                   
                                         <button className='flex justify-center items-center gap-2 text-center text-sm bg-custom-yellow text-white hover:bg-custom-gold px-3 py-2 rounded-3xl w-32' onClick={(e)=>{e.preventDefault();handleClick(data);}}><div >View More</div> <FaAngleRight /></button>
+                                        
                                     </div>
                                 </div>
             
@@ -34,8 +38,8 @@ const TrekkingAll = () => {
             {/* change size here */}
 
 
-                                <div className='hidden lg:block w-1/2'>        
-                                <div className='flex justify-center itemse-center pt-5'><Caroussel imageCarousel={data}/></div>
+                                <div className='hidden lg:block w-2/3'>        
+                                <div className='flex justify-center itemse-center pt-20 w-full'><InnerCarosel imageCarosel={data}/></div>
                                 </div>
                     </div>
                 })
