ada37b5707e6379c740f988da13161fe3a1a3287
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-13 18:08:27 +0545

    review completed

--- src/Components/SubComponent/SubComponent.js
+++ src/Components/SubComponent/SubComponent.js
@@ -1,25 +1,22 @@
-
 import HeroSectionSub from "./HeroSection";
 import Header from "../HomePage/Nav/Header.js";
-import React from 'react'
+import React from "react";
 
-import ContactForm from '../InnerSub/ContactForm/ContactForm.js'
+import ContactForm from "../InnerSub/ContactForm/ContactForm.js";
 
-=======
 import InfoByRegion from "./InfoByRegion/InfoByRegion";
 import TrekkingAll from "./TrekkingAll/TrekkingAll";
 export default function SubComponent() {
-    return (
-        <div>
-        {/*         SubComponent
+  return (
+    <div>
+      {/*         SubComponent
         <Header/>
 
             <HeroSectionSub /> */}
-            <ContactForm />
-        <InfoByRegion/>
-            <TrekkingAll/>
-            <HeroSectionSub />
-        </div>
-    )
-
+      <ContactForm />
+      <InfoByRegion />
+      <TrekkingAll />
+      <HeroSectionSub />
+    </div>
+  );
 }
