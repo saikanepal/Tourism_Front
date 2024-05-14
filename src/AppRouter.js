e3ee38e196081bb0ee1d3894e9b28614251171d9
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-14 21:37:08 +0545

    initial commit

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './Components/HomePage/HomePage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./Components/HomePage/HomePage";
 
-import InnerSub from './Components/InnerSub/InnerSub';
-import SubComponent from './Components//SubComponent/SubComponent.js';
-
+import InnerSub from "./Components/InnerSub/InnerSub";
+import SubComponent from "./Components//SubComponent/SubComponent.js";
+import Admin from "./Components/HomePage/Admin/Admin.js";
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-
-
-                <Route path="/innersub" element={<InnerSub />} /> {/* Replace the component with the appropriate one */}
-                <Route path='/expedition' element={<SubComponent />} />
-                <Route path="/trekking" element={<SubComponent />} /> {/* Replace the component with the appropriate one */}
-                <Route path="/trekking/:region" element={<InnerSub />} />
-                <Route path="/expedition/:region" element={<InnerSub />} />
-                {/* // Replace the component with the appropriate one */}
-                {/*  */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/innersub" element={<InnerSub />} />{" "}
+        {/* Replace the component with the appropriate one */}
+        <Route path="/expedition" element={<SubComponent />} />
+        <Route path="/trekking" element={<SubComponent />} />{" "}
+        {/* Replace the component with the appropriate one */}
+        <Route path="/trekking/:region" element={<InnerSub />} />
+        <Route path="/expedition/:region" element={<InnerSub />} />
+        <Route path="/admin" element={<Admin />} />
+        {/* // Replace the component with the appropriate one */}
+        {/*  */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRouter;
