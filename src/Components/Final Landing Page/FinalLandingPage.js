3f6bde966e062dc29c4563d88ec04f7a1ee2889f
ByapakSigdel <sigdelmb123@gmail.com>
2024-05-17 19:30:59 +0545

    Created the info section for final landing page

--- src/Components/Final Landing Page/FinalLandingPage.js
+++ src/Components/Final Landing Page/FinalLandingPage.js
@@ -1,12 +1,21 @@
 import DataTrekking from '../../Assets/Data/Trekking.js';
 import DataExpedition from '../../Assets/Data/Expedition.js';
 import DataRegion from '../../Assets/Data/Region.js';
+import trekking from './TestData.js';
+import InfoSection from './InfoSection.js';
 
 
 export default function FinalLandingPage() {
+ 
+    const data = trekking.trekking['Everest Region Trekking']['Gokyo Valley Camp Trek'];
+     console.log(data);
+     /* Added this section to test the successful data passing as a prop */
     return (
+        
+        
+
         <div>
-       <p>Final Landing page</p>
+       <InfoSection data = {data} />
         </div>
     )
 
---
+++ src/Components/Final Landing Page/TestData.js
@@ -0,0 +1,46 @@
+const trekking = {
+    "trekking": {
+        "Everest Region Trekking": {
+            "Everest Base Camp Trek": {
+                "MaxElevation": "8848",
+                "Difficulty": "Hard",
+                "Time": "1-2 days",
+                "Region": "Everest Region",
+                "BestSeason": "Spring",
+                "GroupSize": "2-4",
+                "WalkingPerDay": "5-6 hrs"
+            },
+            "Gokyo Valley Camp Trek": {
+                "MaxElevation": "3000",
+                "Difficulty": "Medium",
+                "Time": "1 days",
+                "Region": "Everest Region",
+                "BestSeason": "Spring",
+                "GroupSize": "3",
+                "WalkingPerDay": "4-5-6 hrs"
+            }
+        },
+        "Annapurna Region Trekking": {
+            "Annapurna Base Camp Trek": {
+                "MaxElevation": "8848",
+                "Difficulty": "Hard",
+                "Time": "1-2 days",
+                "Region": "Annapurna Region",
+                "BestSeason": "Spring",
+                "GroupSize": "2-4",
+                "WalkingPerDay": "5-6 hrs"
+            },
+            "Ghorepani Camp Trek": {
+                "MaxElevation": "3000",
+                "Difficulty": "Medium",
+                "Time": "1 days",
+                "Region": "Annapurna Region",
+                "BestSeason": "Spring",
+                "GroupSize": "3",
+                "WalkingPerDay": "4-5-6 hrs"
+            }
+        }
+    }
+};
+
+export default trekking;
