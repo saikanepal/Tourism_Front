0f4477e4a21d1564a9c6f37b03f7cff2a0eacb6c
Bishal Bhatta <bishalbhatta1028@gamil.com>
2024-05-21 20:42:50 +0545

    update image upload page

--- src/Components/HomePage/Nav/HeroSection.js
+++ src/Components/HomePage/Nav/HeroSection.js
@@ -1,35 +1,24 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 import "./CarouselArrow.css";
-import "./style.css";
+// import "./style.css";
 
 function CarouselItem({ imageUrl }) {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/expedition");
+  };
   return (
     <div
       className="h-screen xl:h-screen bg-cover bg-center text-left flex justify-start  "
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-      <div className="sm:w-full lg:w-3/5 xl:1/3 flex justify-center lg:justify-center">
-        <div className="w-2/3 mt-auto mb-auto">
-          <p className="heroText text-5xl mb-3 font-poppins font-bold text-white hover:text-red-400 ">
-            Explore, Elevate, Engage
-          </p>
-
-          <Link
-            to="/expedition"
-            className="flex w-1/3 justify-around items-center learnButton mt-7 bg-secondary font-semibold font-poppins sm:text-base md:text-xl text-[#CA8F30] border-[#CA8F30] border-2 rounded-3xl px-4 py-3 hover:bg-[#CA8F30] hover:text-white pl-1 z-10"
-          >
-            Learn More{" "}
-            <span className="font-bold md:text-[18px] lg:text-[28px]">
-              <IoIosArrowForward />
-            </span>
-          </Link>
-          <button className="bg-red-400 hover:bg-black "> hello world</button>
-        </div>
-      </div>
+      <div className="sm:w-full lg:w-3/5 xl:1/3 flex justify-center lg:justify-center"></div>
     </div>
   );
 }
@@ -71,13 +60,34 @@ function HeroSection() {
         interval={5000}
         infiniteLoop={true}
         autoPlay={true}
-        // stopOnHover={false}
+        stopOnHover={false}
         animationHandler="fade"
       >
         {images.map((image, index) => (
           <CarouselItem key={image._id} imageUrl={image.img} />
         ))}
       </Carousel>
+      {/* <button className="bg-red-400 hover:bg-black  absolute mb:[-50px] ">
+        {" "}
+        hello world
+      </button> */}
+      <div className=" absolute  left-1/3 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 ml-4">
+        <div className="w-2/3 mt-auto mb-auto">
+          <p className="heroText text-5xl mb-3 font-poppins font-bold text-white hover:text-custom-gold ">
+            Explore, Elevate, Engage
+          </p>
+
+          <Link
+            to="/expedition"
+            className=" flex w-1/3 lg:w-1/2 justify-around items-center learnButton mt-7 bg-secondary font-semibold font-poppins sm:text-base md:text-xl text-[#CA8F30] border-[#CA8F30] border-2 rounded-3xl px-4 py-3 hover:bg-[#CA8F30] hover:text-white pl-1 z-10"
+          >
+            Learn More{" "}
+            <span className="font-bold md:text-[18px] lg:text-[28px]">
+              <IoIosArrowForward />
+            </span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
